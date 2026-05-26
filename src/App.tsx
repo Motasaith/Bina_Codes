0a4ccfc14a76205eff8ae74c123f56a938bacfd2
Motasaith <saithmota@gmail.com>
2026-05-26 18:04:14 +0500

    feat: add CTA section with an organic pebble-shaped glass button Build Together on white background

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import About from './components/About';
 import Contact from './components/Contact';
+import Cta from './components/Cta';
 import Footer from './components/Footer';
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
           <About />
           <Contact />
         </div>
+        <Cta />
       </main>
       <Footer />
     </>
